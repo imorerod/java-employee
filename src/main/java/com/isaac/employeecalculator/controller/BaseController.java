3a03e52e1bd79ba0ea5976ba8baf94f88c91b4ea
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-06-21 11:28:01 -0500

    search function added

---
+++ src/main/java/com/isaac/employeecalculator/controller/BaseController.java
@@ -0,0 +1,46 @@
+package com.isaac.employeecalculator.controller;
+
+import com.isaac.employeecalculator.data.EmployeeRepository;
+import com.isaac.employeecalculator.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class BaseController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @RequestMapping(value = "/add/employee", method = RequestMethod.POST)
+    public @ResponseBody Employee addEmployee(@RequestBody Map<String, Object> payload) {
+        String newId = String.valueOf(payload.get("id"));
+        String newFirstName = String.valueOf(payload.get("firstName"));
+        String newLastName = String.valueOf(payload.get("lastName"));
+        String newPosition = String.valueOf(payload.get("position"));
+        String newSalary = String.valueOf(payload.get("salary"));
+
+        Employee newEmployee = new Employee(newId, newFirstName, newLastName, newPosition, newSalary);
+        employeeRepository.addEmployee(newEmployee);
+
+        return newEmployee;
+    }
+
+    @RequestMapping("/get/employees")
+    public @ResponseBody List<Employee> getEmployees() {
+        return employeeRepository.getAllEmployees();
+    }
+
+    @RequestMapping("/search/{id}")
+    public @ResponseBody List<Employee> searchById(@PathVariable String id) {
+        return employeeRepository.searchById(id);
+    }
+
+    @RequestMapping("/")
+    public String baseRoute() {
+        return "index";
+    }
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,80 @@
+$(document).ready(onReady);
+
+function onReady() {
+    $('#btnSearchId').on('click', function(event) {
+        const id = $("#searchId").val();
+
+        $.ajax({
+            type: "GET",
+            url: "/search/" + id,
+            success: function(response) {
+                console.log("Found employee: ", response);
+            }
+         })
+    })
+
+    $('#employeeForm').on('submit', employeeFormSubmit);
+}
+
+let employeeArray = [];
+
+function employeeFormSubmit(event) {
+    event.preventDefault();
+
+    const id = $('.id').val();
+    const firstName = $('.firstName').val();
+    const lastName = $('.lastName').val();
+    const position = $('.position').val();
+    const salary = $('.salary').val();
+
+    const employeeObject = {
+    id,
+    firstName,
+    lastName,
+    position,
+    salary
+    }
+
+    postEmployee(employeeObject);
+
+}
+
+function postEmployee(enteredEmployee) {
+    $.ajax({
+        type: "POST",
+        url: "/add/employee",
+        data: JSON.stringify(enteredEmployee),
+        contentType: "application/json; charset=utf-8",
+        beforeSend: function() {
+            console.log('hi')
+        },
+        success: function(response) {
+            console.log(response);
+            getEmployees();
+        }
+    })
+}
+
+function getEmployees() {
+    $.ajax({
+        type: "GET",
+        url: "/get/employees",
+        success: function(response) {
+            console.log(response);
+        }
+    })
+}
+
+function redrawEmployeeInfo() {
+    $('#container').empty();
+    for (let i = 0; i < employeeArray.length; i++) {
+        $('#container').append('<div></div>');
+        let divJustCreated = $('#container').children().last();
+        let employee = employeeArray[i];
+        divJustCreated.append('<p>' + employee.id + '</p>');
+        divJustCreated.append('<p>' + employee.firstName + '</p>');
+        divJustCreated.append('<p>' + employee.lastName + '</p>');
+        divJustCreated.append('<p>' + employee.position + '</p>');
+        divJustCreated.append('<p>' + employee.salary + '</p>');
+    }
+}
